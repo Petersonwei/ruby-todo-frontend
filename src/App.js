f474de645b3bba7f85db2ec63864eb607aecd3e4
Pete <pwei.dev@gmail.com>
2025-06-29 17:19:46 +1000

    add the useState useEffect to the App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import './App.css';
 import TodoList from './components/TodoList';
 
+import { useState, useEffect } from 'react';
+
+import { getTodos } from './api/endpoints';
+
 function App() {
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const fetchTodos = async () => {
+      const todos = await getTodos();
+      setTodos(todos);
+      console.log(todos);
+    }
+    fetchTodos();
+  }, []);
+
   return (
     <div className="App">
       <div className="app-container">

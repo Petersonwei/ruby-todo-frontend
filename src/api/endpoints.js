3929f3182a6a42f7993442684781de7de2dab5e0
Pete <pwei.dev@gmail.com>
2025-06-29 18:08:54 +1000

    add POST endpoint

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:3000/api';
 const GET_URL = `${API_URL}/todos`;
+const POST_URL = `${API_URL}/todos`;
 
 export const getTodos = async () => {
     const response = await axios.get(GET_URL);
     return response.data;
+}
+
+export const createTodo = async (todo_name) => {
+    const response = await axios.post(POST_URL, { 'todo_name': todo_name, 'completed': false });
 }
